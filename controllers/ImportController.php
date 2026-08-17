da4e41fb64bde554e54ed81bd996771847038d3b
reynaldocv <reynaldocv@gmail.com>
2026-08-17 14:09:09 -0300

    123123

--- controllers/ImportController.php
+++ controllers/ImportController.php
@@ -103,7 +103,7 @@ class ImportController extends ActionController {
 				$o_entity->update(); 
 			}
 		}
-		$o_entity->replaceAttribute(array($next_Parameter => "MACUSP exhibitions"),$next_Parameter);	
+		$o_entity->replaceAttribute(array($next_Parameter => "MACUSP Exhibitions"),$next_Parameter);	
 		$o_entity->update(); 
 				
 		$o_entity2 = new ca_occurrences($id);
