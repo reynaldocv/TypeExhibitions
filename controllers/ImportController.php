ad80bb171e00dcc2ae1d766749a4993c9ccf2c4a
reynaldocv <reynaldocv@gmail.com>
2026-08-17 13:39:25 -0300

    123

--- controllers/ImportController.php
+++ controllers/ImportController.php
@@ -94,7 +94,8 @@ class ImportController extends ActionController {
 
 		$type = $o_entity ->getWithTemplate("^ca_occurrences.$prev_Parameter", array('locale' => 'en_US'));
 
-		$o_entity->replaceAttribute(array($next_Parameter => Null),$next_Parameter);	
+		//$o_entity->replaceAttribute(array($next_Parameter => Null),$next_Parameter);	
+		$o_entity->removeAttribute($next_Parameter);	
 
 		/*foreach ($values as [$prev, $next]) {
 			if ($type === $prev)
