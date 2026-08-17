2d501e4de7338b5463b2bebe5e30da2c97439192
reynaldocv <reynaldocv@gmail.com>
2026-08-17 13:36:06 -0300

    123

--- controllers/ImportController.php
+++ controllers/ImportController.php
@@ -96,13 +96,13 @@ class ImportController extends ActionController {
 
 		$o_entity->replaceAttribute(array($next_Parameter => ""),$next_Parameter);	
 
-		foreach ($values as [$prev, $next]) {
+		/*foreach ($values as [$prev, $next]) {
 			if ($type === $prev)
 			{
     			$o_entity->replaceAttribute(array($next_Parameter => $next),$next_Parameter);	
 				$o_entity->update(); 
 			}
-		}
+		}*/
 				
 		$o_entity2 = new ca_occurrences($id);
 		$data = array(); 
